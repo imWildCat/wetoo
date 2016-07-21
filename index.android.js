c7c2385771c7b45d4b9ddeaac69f8cc3b1eaa0c7
WildCat <wildcat.name@gmail.com>
2016-07-21 10:13:30 +0800

    Add support for BackAndroid

--- index.android.js
+++ index.android.js
@@ -6,8 +6,14 @@
 
 import {
   AppRegistry,
+  BackAndroid,
 } from 'react-native';
+import {Actions} from 'react-native-router-flux';
 
 import Wetoo from './src/app';
 
 AppRegistry.registerComponent('Wetoo', () => Wetoo);
+
+BackAndroid.addEventListener('hardwareBackPress', function() {
+  return Actions.pop();
+});
