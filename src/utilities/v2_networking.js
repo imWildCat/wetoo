7bbbfc2cb6494336c0e79538be4f2d06b35fab37
WildCat <wildcat.name@gmail.com>
2016-07-25 18:24:58 +0800

    Improve V2Networking, add support for post, header and option merging

--- src/utilities/v2_networking.js
+++ src/utilities/v2_networking.js
@@ -1,19 +1,40 @@
 import cheerio from 'cheerio';
 
 const V2Networking = {
-  get: (uri, otherConfig = {}) => {
-    return V2Networking.request('GET', uri, otherConfig);
+  get: (uri) => {
+    return V2Networking.request('GET', uri);
   },
-  request: (method, uri, otherConfig = {}) => {
+  post: (uri, data, additionalHeaders = null) => {
+    const headers = Object.assign({ 'Content-Type': 'application/x-www-form-urlencoded' }, additionalHeaders);
+    return V2Networking.request(
+      'POST',
+      uri,
+      headers,
+      { body: serializeJSON2FormData(data) }
+    );
+  },
+  request: (method, uri, additionalHeaders = null, additionalOptions = null) => {
     // TODO: Check user's setting for http/https
-    return fetch(`https://www.v2ex.com${uri}`, {
-      method: method,
-      headers: {
-        'Accept': 'text/html',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
-      }
-    }, ...otherConfig).then(res => res.text()).then(text => cheerio.load(text, { decodeEntities: false }));
+
+    const headers = Object.assign({
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+      'Connection': 'keep-alive',
+    }, additionalHeaders);
+
+    const options = Object.assign({
+      method,
+      headers,
+    }, additionalOptions);
+
+    return fetch(`https://www.v2ex.com${uri}`, options).then(res => res.text()).then(text => cheerio.load(text, { decodeEntities: false }));
   }
 };
+
+function serializeJSON2FormData(data) {
+  return Object.keys(data).map(function (keyName) {
+    return `${encodeURIComponent(keyName)}=${encodeURIComponent(data[keyName])}`;
+  }).join('&');
+}
 
 export default V2Networking;
