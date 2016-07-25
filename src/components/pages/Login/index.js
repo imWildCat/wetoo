185269a044631638de923aa5c42a218489af4c74
WildCat <wildcat.name@gmail.com>
2016-07-25 15:18:48 +0800

    Implement all of the UI of LoginPage

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -4,20 +4,32 @@ import {Actions} from 'react-native-router-flux';
 import Button from 'react-native-button';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import Networking from '../../../utilities/v2_networking';
 import Style from '../../../utilities/style';
 
 const CLOSE_BUTTON_SIZE = 27;
 
 class LoginPage extends Component {
-  static state = {};
+
+  state = {
+    username: null,
+    password: null,
+    isInfoWrong: false,
+    isLoading: false,
+    errorText: null,
+  };
 
   constructor(props) {
     super(props);
 
+    this.focusPasswordField = this.focusPasswordField.bind(this);
     this.onCloseButtonPress = this.onCloseButtonPress.bind(this);
+    this.onSubmitButtonPress = this.onSubmitButtonPress.bind(this);
   }
 
   render() {
+    const { username, password, isInfoWrong, isLoading, errorText } = this.state;
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="default" />
@@ -29,16 +41,67 @@ class LoginPage extends Component {
         </View>
 
         <View style={styles.loginFormWrapper}>
-          <TextInput style={styles.formInput} keyboardType="email-address" autoCapitalize="none" defaultValue={'123'} />
+          <View style={styles.formInputWrapper}>
+            <TextInput placeholder="Email 或 用户名" style={styles.formInput} keyboardType="email-address"
+                       ref="usernameField"
+                       autoCapitalize="none"
+                       autoCorrect={false}
+                       returnKeyType="next"
+                       onChangeText={(username) => this.setState({ username })}
+                       onSubmitEditing={this.focusPasswordField}
+                       underlineColorAndroid="#FFFFFF" />
+          </View>
+          <View style={[styles.formInputWrapper, styles.formInputWrapperForPassword]}>
+            <TextInput placeholder="密码" style={styles.formInput} keyboardType="default"
+                       ref="passwordField"
+                       autoCapitalize="none" secureTextEntry={true}
+                       onChangeText={(password) => this.setState({ password })}
+                       onSubmitEditing={this.onSubmitButtonPress}
+                       underlineColorAndroid="#FFFFFF" />
+          </View>
         </View>
 
-        <Button style={styles.loginButtonText} containerStyle={styles.loginButtonContainer}>登录</Button>
+        <View style={styles.loginFeedbackWrapper}>
+          <Text style={styles.loginFeedbackText}>{errorText}</Text>
+        </View>
+
+        <Button onPress={this.onSubmitButtonPress}
+                disabled={isLoading}
+                style={styles.loginButtonText}
+                containerStyle={[styles.loginButtonContainer, isLoading ? styles.loginButtonContainerDisabled : {}]}>
+          {isLoading ? '登录中...' : '登录'}
+        </Button>
       </View>
     );
   }
 
+  focusPasswordField() {
+    this.refs['passwordField'].focus();
+  }
+
   onCloseButtonPress() {
     Actions.pop();
+  }
+
+  onSubmitButtonPress() {
+    const { isLoading, username, password } = this.state;
+    if (!isLoading) {
+      this.refs['usernameField'].blur();
+      this.refs['passwordField'].blur();
+
+      this.setState({ isLoading: true });
+
+
+    }
+  }
+
+  async getPostToken() {
+    try {
+      let $ = await Networking.get('/signin');
+
+    } catch (error) {
+      
+    }
   }
 
 }
@@ -63,8 +126,7 @@ const styles = Style.create({
     flex: 1,
     textAlign: 'center',
   },
-  closeButtonWrapper: {
-  },
+  closeButtonWrapper: {},
   closeButton: {
     width: CLOSE_BUTTON_SIZE,
     height: CLOSE_BUTTON_SIZE,
@@ -72,14 +134,30 @@ const styles = Style.create({
 
   loginFormWrapper: {
     marginTop: 120,
-    marginBottom: 50,
+    marginBottom: 15,
+  },
+  formInputWrapper: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#D8E0E4',
+  },
+  formInputWrapperForPassword: {
+    marginTop: 18,
   },
   formInput: {
-    borderWidth: 1,
-    borderColor: 'red',
     flex: 1,
     height: 30,
-    marginBottom: 20,
+    android: {
+      height: 33,
+    }
+  },
+
+  loginFeedbackWrapper: {
+    height: 20,
+    marginBottom: 15,
+  },
+  loginFeedbackText: {
+    color: 'red',
+    textAlign: 'center',
   },
 
   loginButtonContainer: {
@@ -87,6 +165,9 @@ const styles = Style.create({
     borderRadius: 5,
     backgroundColor: '#73BCF1',
     justifyContent: 'center',
+  },
+  loginButtonContainerDisabled: {
+    backgroundColor: '#B9DDF8',
   },
   loginButtonText: {
     color: '#FFFFFF',
